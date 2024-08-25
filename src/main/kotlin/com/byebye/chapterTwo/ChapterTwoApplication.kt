2a64b31856c07b523615f51b27141a6c0fff7987
Sewook <sewook0303@dgsw.hs.kr>
2024-08-26 00:19:38 +0900

    server setting

---
+++ src/main/kotlin/com/byebye/chapterTwo/ChapterTwoApplication.kt
@@ -0,0 +1,11 @@
+package com.byebye.chapterTwo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChapterTwoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChapterTwoApplication>(*args)
+}
---
+++ src/test/kotlin/com/byebye/chapterTwo/ChapterTwoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.byebye.chapterTwo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChapterTwoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
